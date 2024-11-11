8dc43e7667a43fbdbf0841184f9ae586b51920b7
ignacioTech <ignaciobarboza1983@gmail.com>
2024-11-10 22:38:46 -0300

    alta producto + visualizacion

--- index.py
+++ index.py
@@ -3,6 +3,7 @@
 productos = []
 #Menu
 while True :
+    print("")
     print("Gestion de Productos")
     print("")
     print("1- Registro: Ingreso de Productos")
@@ -15,21 +16,71 @@ while True :
 
     #PROCESO ENTRADA OPCIONES
     try:
-        opcion = int(input("Ingrese una opción del 1 - 6 || 0(cero) para salir"))
+        opcion = int(input("Ingrese una opción del 1 - 6 || 0(cero) para salir "))
 
         if opcion == 0:
             print("Salir")
             break
+
         elif opcion == 1:
             print("1-Registro-")
+            print("")
+            registro = False
+            while registro == False:
+                try:
+                    nombre = input("Ingrese Nombre o 0 (Cero) para ir al menú anterior ")
+                    if nombre == "0":
+                        break
+                    else:
+                        while registro == False:
+                            try:
+                                cantidad = int(input("Ingrese Cantidad - 0 (cero) para volver al menu anterior "))
+                                if cantidad == 0:
+                                    break
+                                else:
+                                    while registro == False:
+                                        try:
+                                            precio = float(input("Ingrese Precio  - 0 (cero) para volver al menu anterior "))
+                                            if precio == 0:
+                                                break
+                                            else:
+                                                producto = [nombre, cantidad, precio]
+                                                try: 
+                                                    confirmacion = input("Confirma el ingreso del precio? s/n ")
+
+                                                    if confirmacion.lower() == "s":
+                                                        productos.append(producto)
+                                                        print("El producto: ", producto, " Ha ingresado al stock ")
+                                                        registro = True
+                    
+                                                except ValueError:
+                                                    print("Ingreso no valido")
+                                        except ValueError:
+                                            print("Ingreso no valido")
+                            except ValueError:
+                                print("Ingreso no valido")
+                except ValueError:
+                    print("Ingreso no valido")
+                try:
+                    seguir = input("desea ingresar otro producto? s/n ")
+                    
+                    if seguir.lower() == "s":
+                        registro = False 
+                except ValueError:
+                    print("Ingreso no valido")
+
         elif opcion == 2:
             print("2-Visualizacion-")
+            
         elif opcion == 3:
             print("3-Actualizacion-")
         elif opcion == 4:
             print("4-Eliminacion-")
         elif opcion == 5:
             print("5-Listado-")
+            print("")
+            for producto in productos:
+                print(producto)
         elif opcion == 6:
             print("6-Reporte Bajo Stock-")
         else:
